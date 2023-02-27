69819f6ec6981d77ef98fcbffd04728058a21702
yubn21 <125863226+yubn21@users.noreply.github.com>
2023-02-27 14:55:34 +0100

    2 files modified

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent {
+  contactForm = this.formBuilder.group({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    Swal.fire('Success', 'Your message has been sent', 'success');
+  }
 }
